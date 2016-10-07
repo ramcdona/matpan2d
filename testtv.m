98c0ec87ef3dadfe287d6c8d96a2e7654a8cb506
Rob McDonald <rob.a.mcdonald@gmail.com>
2016-10-07 14:41:50 -0700

    Add semi-infinite tube vortex influence function from Lewis.

---
+++ testtv.m
@@ -0,0 +1,25 @@
+clear all
+format compact
+
+nx = 20;
+nr = 13;
+
+xs = linspace( -5, 5, nx );
+rs = linspace( 0, 4, nr );
+
+x0 = 0;
+r0 = 1;
+
+u = nan( nr, nx );
+v = u;
+
+[x,r] = meshgrid( xs, rs );
+
+for i=1:nx
+    for j=1:nr
+        [ u(j,i), v(j,i) ] = tubevortex( x0, r0, xs(i), rs(j) );
+    end
+end
+
+figure(1)
+quiver( x, r, u, v );
---
+++ tubevortex.m
@@ -0,0 +1,36 @@
+function [ u, v ] = tubevortex( xn, rn, xm, rm )
+% Velocity induced by unit-strength semi-infinite (downstream) tube vortex
+% around the x-axis.
+%
+% Vortex Element Methods for Fluid Dynamic Analysis of Engineering Systems
+% R. I. Lewis, 1991, Cambridge University Press, p. 169
+%
+% n Subscript for the ring vortex
+% m Subscript for the point of interest
+
+% Non-dimensional coordinates
+x = (xm - xn)./rn;
+r = rm./rn;
+
+reqone = ( r == 1 );
+
+A = zeros( size(r) );
+A( r < 1 ) = pi;
+A( reqone ) = pi/2.0;
+
+C = x.^2 + ( r + 1.0 ).^2;
+
+% Matlab's elliptic integral routine is done in terms of M, where M=k^2
+ksq = 4.0 * r ./ C;
+[K,E] = ellipke( ksq );
+
+n = ( 4.0 * r ) ./ ( r + 1.0 ).^2;
+
+PI = ellipticPi( n, ksq );
+
+v = 2.0 ./ ( pi .* ksq .* sqrt( C ) ) .* ( E - K .* (1.0 - ksq ./ 2 ) );
+
+u = zeros( size (v) );
+u( ~reqone ) = 1.0 ./ ( 2.0 * pi ) .* ( A(~reqone) + ( x(~reqone) ./ sqrt( C(~reqone) ) ) .* ( K(~reqone) - PI(~reqone) .* ( r(~reqone) - 1.0 ) ./ ( r(~reqone) + 1.0 ) ) );
+u( reqone ) = 0.25 + x(reqone) .* K(reqone) ./ ( 2.0 * pi .* sqrt( x(reqone).^2 + 4.0 ) );
+end
